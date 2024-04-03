2de145837316e1a3228acfc279c59e4d363312f6
kehetthoi <vohuy.76.thcsnvb2122@gmail.com>
2024-04-04 02:26:58 +0700

    hi

---
+++ bt1.py
@@ -0,0 +1,117 @@
+#user_name = input("nhập tên mày: ")
+#print("hi, " + user_name )
+#ho=input("nhập họ: ")
+#ten=input("nhập tên(kèm tên đệm): ")
+#sdt=input("nhập số điện thoại: ")
+#print("Họ: "+ ho )
+#print("Tên(có tên đệm): "+ ten )
+#print("Số điện thoại: "+ sdt )
+#print("Tên đầy đủ cuả bạn là "+ ho +" " + ten)
+#print("Số điện thoại của bạn là "+ sdt)
+#name = "huy"
+#birthdate = "01/02/2009"
+#print("Tên\t\t: "+ name)
+#print("Sinh nhật\t: "+ birthdate)
+"""
+def print_fireworks(year):
+    fireworks = [
+        "                      . : .",
+        "                    '.  :  .'",
+        "  HAPPY NEW YEAR   .  '.:.'  .",
+        "   !!! {} !!!    .  .':'.  .",
+        "                    .'  :  '.",
+        "                      ' : '"
+    ]
+    for line in fireworks:
+        
+            print(line.format(year))
+        
+
+
+def main():
+    year = input("What year is it?\n")
+    if len(year) == 4 and year.isdigit():
+        print_fireworks(year)
+    else:
+        print("nhập số tk ngu")
+
+
+if __name__ == "__main__":
+    main()
+"""
+'''
+from turtle import *
+pen
+forward(90)
+right(-120)
+forward(90)
+forward(90)
+right(-120)
+forward(90)
+forward(90)
+right(-120)
+forward(90)
+
+mainloop()
+'''
+'''
+from turtle import *
+
+
+pen
+pensize(3)
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+penup()
+goto(10,-10)
+pendown()
+pensize(1)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+right(90)
+
+mainloop()
+'''
+from turtle import *
+pen
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+penup()
+goto(45,20)
+pendown()
+setheading(45)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+right(90)
+forward(90)
+mainloop()
+
+
+
+
+
+
+
+
+
