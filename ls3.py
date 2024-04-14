3c857c89f8377f31663f3dc90b105a7362de9a5f
kehetthoi <vohuy.76.thcsnvb2122@gmail.com>
2024-04-14 12:07:47 +0700

    rdsdfdsf

---
+++ ls3.py
@@ -0,0 +1,80 @@
+'''
+a=int(input("nhap so :"))
+if a % 2 == 0:
+    print("so tren la so chan")
+elif a%2 != 0:
+    print("so tren la so le")
+'''
+'''
+i = 1
+b = 0
+while i < 101:
+  b+=i
+  i += 1
+print(b)
+'''
+'''
+a=int(input("nhap so:"))
+b=int(input("nhap so:"))
+c=int(input("nhap so:"))
+
+if a > b and a > c  :
+    print("a lon nhat")
+
+
+if  b > c and b > a :
+    print("b lon nhat")
+if c>a and c>b:
+    print("c lon nhat")
+    '''
+'''
+a=int(input("nhap so ngay trong nam:"))
+if a%400==0 and a%100==00:
+    print("nam do la nam nhuan")        
+else:
+    print("nam do ko phai nam nhuan")
+    '''
+'''
+for i in range(7):
+    if i <= 6:
+        print(i)
+        '''
+'''
+a=100
+while a<106:
+    print(a)
+    a+=1
+    '''
+'''
+for x in range(9,1,-1):
+    print(x)
+    '''
+'''
+for i in range(21):  
+    if i % 3 == 0:   
+        print(i) 
+        '''
+
+a = int(input("nhap so nguyen: "))  
+b = str(abs(a)) 
+c = len(b)   
+print("so chu so cua", a, "la:", c)
+
+'''
+a=int(input("nhap so:"))
+b=int(input("nhap so:"))
+c=int(input("nhap so:"))
+max=a
+num=[a,b,c]
+for i in num:
+    if i > max:
+        max = i
+print(max)
+'''
+
+
+
+  
+
+  
+
