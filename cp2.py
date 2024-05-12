2939a128a393a0fd1bf7d5e44f8059c45af9d092
kehetthoi <vohuy.76.thcsnvb2122@gmail.com>
2024-05-12 11:45:31 +0700

    hi

---
+++ cp2.py
@@ -0,0 +1,128 @@
+#phan1
+#bai1
+# a=["do","xanh","vang"]
+#bai2
+# a=["do","xanh","vang"]
+# print("bang mau:",a)
+#bai3
+# a=["do","xanh","vang"]
+# print("bang mau:",a)
+# b=input("ban muon them mau gi:")
+# a.append(b)
+# print("bang mau moi:",a)
+
+#phan2
+#bai1
+# a=["do","xanh","vang"]
+# print(a)
+# b=int(input("nhap vi tri cua mau bang so"))
+# print(b)
+# if b == 1:
+#     print("mau cua ban la:do")
+# elif b==2:
+#     print("mau cua ban l:xanh")
+# elif b==3:
+#     print("mau cua ban la:vang")
+#bai2
+# a=["do","xanh","vang"]
+# for i in range(len(a)):
+#     print(a[i])
+# b=input("ban muon bo mau nao:")
+# print(b)
+# if b in a:
+#     a.remove(b)
+#     print("ban do bo mau:",b)
+#     print(a)
+# else:
+#     print("mau nay ko co trong bang")
+#bai3
+
+#phan3
+#bai1
+# a=[5,8,4,9,10]
+# b=int(input("nhap 1 mot nguyen:"))
+# print(b)
+# if b in a:
+#     vitri=a.index(b)+1
+#     print(f"vi tri cua {b} la: ",vitri)
+# else:
+#     print("so khong tom tai")
+#bai2
+# a=[5,8,4,9,10]
+# b=sum(a)
+# print("tong cua listla:",b)
+#bai3
+# numbers = []
+# while True:
+#     number = int(input("Nhập một số nguyên (nhập 0 để kết thúc): "))
+#     if number == 0:
+#         break  
+#     numbers.append(number)
+# total = sum(numbers)
+# print("Tổng của dãy số là:", total)
+
+
+
+#phan4
+#bai1
+# a=[2,8,9,6]
+# for b in range(len(a)):
+#     if a[b]%2==0:
+#         print(a[b])
+#bai2:
+# numbers = []
+# while True:
+#     number = int(input("Nhập một số nguyên (nhập 0 để kết thúc): "))
+#     if number == 0:
+#         break  
+#     numbers.append(number)
+# for number in range(len(numbers)):
+#     if numbers[number]%2==0:
+#      print(numbers[number])
+
+#phan5
+#bai1
+# a=["hcm","hn","th"]
+# b=[245,455,123]
+
+#phan7
+# diemcao=[78,84,96]
+# diemcao.sort(reverse = True)
+# for i in range(len(diemcao)):
+#     print(f"{i+1}:",diemcao[i])
+#bai3:
+diem_cao = [78, 56, 67]
+diem_moi = int(input("Nhập điểm mới của người chơi: "))
+diem_cao.append(diem_moi)
+
+print("Điểm mới đã được thêm vào danh sách điểm cao.")
+print("Danh sách điểm cao mới:")
+print(diem_cao)
+
+#phan8
+#bai1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
